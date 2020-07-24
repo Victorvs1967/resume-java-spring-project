d016703aaa1e59556c47670a5d2c83b99ffd4387
Victor <victorsmirnov@me.com>
2020-07-24 18:26:23 +0300

    create app

---
+++ src/main/java/com/vvs/resume/model/LanguageLevel.java
@@ -0,0 +1,16 @@
+package com.vvs.resume.model;
+
+public enum LanguageLevel {
+
+	BEGINNER,
+	ELEMENTARY,
+	PRE_INTERMEDIATE,
+	INTERMEDIATE,
+	UPPER_INTERMEDIATE,
+	ADVANCED,
+	PROFICIENCY;
+	
+	public String getDbValue(){
+		return name().toLowerCase();
+	}
+}
