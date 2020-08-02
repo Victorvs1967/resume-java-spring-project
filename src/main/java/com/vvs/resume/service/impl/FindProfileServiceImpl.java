95ca05ce7f3790235f2bbd6f69544707b768a770
Victor <victorsmirnov@me.com>
2020-08-02 18:56:37 +0300

    add Services

---
+++ src/main/java/com/vvs/resume/service/impl/FindProfileServiceImpl.java
@@ -0,0 +1,27 @@
+package com.vvs.resume.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vvs.resume.entity.Profile;
+import com.vvs.resume.repository.storage.ProfileRepository;
+import com.vvs.resume.service.FindProfileService;
+
+@Service
+public class FindProfileServiceImpl implements FindProfileService {
+
+	@Autowired
+	private ProfileRepository profileRepository;
+	
+	@Override
+	public Profile findByUid(String uid) {
+
+		return profileRepository.findByUid(uid);
+	}
+	
+	@Override
+	public Iterable<Profile> findAll() {
+		
+		return profileRepository.findAll();
+	}
+}
