86434edd8cddf6c81f5b1ed310435166b7338220
Victor <victorsmirnov@me.com>
2020-08-03 17:23:40 +0300

    add services

--- src/main/java/com/vvs/resume/repository/storage/ProfileRepository.java
+++ src/main/java/com/vvs/resume/repository/storage/ProfileRepository.java
@@ -10,6 +10,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.vvs.resume.entity.Profile;
 
 public interface ProfileRepository extends PagingAndSortingRepository<Profile, Long> {
+	
+	default Profile findOne(Long id) {
+		return findById(id).get();
+	}
 
 	Profile findByUid(String uid);
 	
